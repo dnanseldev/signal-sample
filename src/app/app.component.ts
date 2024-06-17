666316bbd25bce583597a60c6f3c3909a10230d0
dnanseldev <dnansel.dev@gmail.com>
2024-06-17 16:25:53 -0300

    Example of Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'signal-sample';
+
+  protected sampleSignal = signal('');
+  protected exampleCount = signal(0)
+ protected computedSample = computed(() => `${this.exampleCount()} + computed`)
+
+
+  fireOff() {
+    //this.sampleSignal.set('Das is ein signal')
+    this.exampleCount.update(cur => cur + 1)
+  }
 }
