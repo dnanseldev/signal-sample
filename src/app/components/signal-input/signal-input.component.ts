a4069aa0c42e26184549d1771a8813ee56fa283b
danstonebuilt <dnansel.dev@gmail.com>
2024-06-19 12:56:17 -0300

    Small improvements

--- src/app/components/signal-input/signal-input.component.ts
+++ src/app/components/signal-input/signal-input.component.ts
@@ -1,12 +1,16 @@
-import { Component, input } from '@angular/core';
+import { Component, input } from "@angular/core";
 
 @Component({
-  selector: 'signal-input',
+  selector: "app-signal-input",
   standalone: true,
   imports: [],
-  templateUrl: './signal-input.component.html'
+  templateUrl: "./signal-input.component.html",
 })
 export class SignalInputComponent {
   //label = input("Daniel Stonebuilt")
-  label = input.required<string>()
+  label = input.required<string>();
+  label2 = input.required<string>({
+    alias: "title2",
+  });
+  title = input("Daniel Stonebuilt");
 }
